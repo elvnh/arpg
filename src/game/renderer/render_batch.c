3ae49b7d72a44a4f4cb6b9f89368a7f6e1833ac0
elvin <135953306+elvnh@users.noreply.github.com>
2025-09-09 09:01:26 +0200

    Reduce code duplication when pushing render commands

--- src/game/renderer/render_batch.c
+++ src/game/renderer/render_batch.c
@@ -1,37 +1,64 @@
 #include "render_batch.h"
 #include "base/linear_arena.h"
+#include "base/utils.h"
 #include "render_command.h"
+
+#define allocate_render_cmd(arena, kind) init_render_cmd(arena, kind)
+
+static void *allocate_render_cmd(LinearArena *arena, RenderCmdKind kind)
+{
+    void *result = 0;
+
+    switch (kind) {
+        case RENDER_CMD_SPRITE: {
+            result = la_allocate_item(arena, SpriteCmd);
+        } break;
+
+        case RENDER_CMD_RECTANGLE: {
+            result = la_allocate_item(arena, RectangleCmd);
+        } break;
+
+        INVALID_DEFAULT_CASE;
+    }
+
+    ASSERT(result);
+    ((RenderCmdHeader *)result)->kind = kind;
+
+    return result;
+}
+
+static RenderEntry *push_render_entry(RenderBatch *rb, RenderKey key, void *data)
+{
+    RenderEntry *entry = &rb->entries[rb->entry_count++];
+    entry->key = key;
+    entry->data = data;
+
+    return entry;
+}
 
 RenderEntry *render_batch_push_sprite(RenderBatch *rb, LinearArena *arena, TextureHandle texture,
     Rectangle rectangle, ShaderHandle shader, s32 layer)
 {
-    SpriteCmd *sprite = la_allocate_item(arena, SpriteCmd);
-    sprite->header.kind = RENDER_CMD_SPRITE;
+    SpriteCmd *sprite = allocate_render_cmd(arena, RENDER_CMD_SPRITE);
     sprite->rect = rectangle;
 
     RenderKey key = render_key_create(layer, shader, texture, (s32)rectangle.position.y);
-    RenderEntry *entry = &rb->entries[rb->entry_count++];
-    entry->key = key;
-    entry->data = (RenderCmdHeader *)sprite;
+    RenderEntry *result = push_render_entry(rb, key, sprite);
 
-    return entry;
+    return result;
 }
 
 RenderEntry *render_batch_push_quad(RenderBatch *rb, LinearArena *arena, Rectangle rect,
     RGBA32 color, ShaderHandle shader, s32 layer)
 {
-    // TODO: reduce code duplication
-    RectangleCmd *cmd = la_allocate_item(arena, RectangleCmd);
-    cmd->header.kind = RENDER_CMD_RECTANGLE;
+    RectangleCmd *cmd = allocate_render_cmd(arena, RENDER_CMD_RECTANGLE);
     cmd->rect = rect;
     cmd->color = color;
 
     RenderKey key = render_key_create(layer, shader, NULL_TEXTURE, (s32)rect.position.y);
-    RenderEntry *entry = &rb->entries[rb->entry_count++];
-    entry->key = key;
-    entry->data = (RenderCmdHeader *)cmd;
+    RenderEntry *result = push_render_entry(rb, key, cmd);
 
-    return entry;
+    return result;
 }
 
 void render_entry_set_uniform_vec4(RenderEntry *re, LinearArena *arena, String uniform_name, Vector4 vec)

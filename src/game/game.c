05b9b6fbbbd1aed6cb30ff1f7d4d376109a89f54
elvin <135953306+elvnh@users.noreply.github.com>
2025-09-14 18:39:00 +0200

    Only reset velocity in direction of collision

--- src/game/game.c
+++ src/game/game.c
@@ -132,9 +132,23 @@ static f32 ray_vs_line_intersection_fraction(Vector2 ray_origin, Vector2 ray_end
     return INFINITY;
 }
 
-static f32 rect_min_ray_intersection_fraction(Rectangle rect, Vector2 origin, Vector2 direction)
+typedef enum {
+    RECT_SIDE_TOP,
+    RECT_SIDE_RIGHT,
+    RECT_SIDE_BOTTOM,
+    RECT_SIDE_LEFT,
+} RectangleSide;
+
+typedef struct {
+    b32 is_intersecting;
+    f32 time_of_impact;
+    RectangleSide side_of_collision;
+} RectRayIntersection;
+
+// TODO: don't provide direction, provide end
+static RectRayIntersection rect_shortest_ray_intersection(Rectangle rect, Vector2 ray_origin, Vector2 ray_direction)
 {
-    Vector2 end = v2_add(origin, direction);
+    Vector2 end = v2_add(ray_origin, ray_direction);
 
     Line left = {
         rect.position,
@@ -156,13 +170,47 @@ static f32 rect_min_ray_intersection_fraction(Rectangle rect, Vector2 origin, Ve
         v2(rect.position.x + rect.size.x, rect.position.y)
     };
 
-    f32 min_fraction = ray_vs_line_intersection_fraction(origin, end, left);
+    RectangleSide side_of_collision = 0;
+    f32 min_fraction = INFINITY;
+    f32 x = min_fraction;
 
-    min_fraction = MIN(min_fraction, ray_vs_line_intersection_fraction(origin, end, top));
-    min_fraction = MIN(min_fraction, ray_vs_line_intersection_fraction(origin, end, right));
-    min_fraction = MIN(min_fraction, ray_vs_line_intersection_fraction(origin, end, bottom));
+    x = ray_vs_line_intersection_fraction(ray_origin, end, left);
 
-    return min_fraction;
+    if (x < min_fraction) {
+        min_fraction = x;
+        side_of_collision = RECT_SIDE_LEFT;
+    }
+
+    x = MIN(min_fraction, ray_vs_line_intersection_fraction(ray_origin, end, top));
+
+    if (x < min_fraction) {
+        min_fraction = x;
+        side_of_collision = RECT_SIDE_TOP;
+    }
+
+    x = MIN(min_fraction, ray_vs_line_intersection_fraction(ray_origin, end, right));
+
+    if (x < min_fraction) {
+        min_fraction = x;
+        side_of_collision = RECT_SIDE_RIGHT;
+    }
+
+    x = MIN(min_fraction, ray_vs_line_intersection_fraction(ray_origin, end, bottom));
+
+    if (x < min_fraction) {
+        min_fraction = x;
+        side_of_collision = RECT_SIDE_BOTTOM;
+    }
+
+    RectRayIntersection result = {0};
+
+    if (min_fraction != INFINITY) {
+        result.is_intersecting = true;
+        result.side_of_collision = side_of_collision;
+        result.time_of_impact = min_fraction;
+    }
+
+    return result;
 }
 
 static Rectangle minkowski_diff(Rectangle a, Rectangle b)
@@ -235,7 +283,8 @@ static void world_update(GameWorld *world, const Input *input, f32 dt)
 
     for (s32 i = 0; i < world->entity_count; ++i) {
         Entity *a = &world->entities[i];
-        Vector2 velocity_this_frame = a->velocity;
+
+        f32 movement_fraction_left = 1.0f;
 
         for (s32 j = i + 1; j < world->entity_count; ++j) {
             Entity *b = &world->entities[j];
@@ -248,20 +297,20 @@ static void world_update(GameWorld *world, const Input *input, f32 dt)
 
             if (rect_contains_point(md, V2_ZERO) && (v2_mag(collision_vector) > INTERSECTION_EPSILON)) {
                 // Have already collided
-                printf("c\n");
+                // TODO: should only velocity in collision direction be reset here?
                 a->position = v2_sub(a->position, collision_vector);
-
-                velocity_this_frame = V2_ZERO;
 
                 a->velocity = V2_ZERO;
                 b->velocity = V2_ZERO;
             } else {
                 Vector2 relative_velocity = v2_mul_s(v2_sub(b->velocity, a->velocity), dt);
-                f32 intersect_fraction = rect_min_ray_intersection_fraction(md, V2_ZERO, relative_velocity);
+                RectRayIntersection intersection = rect_shortest_ray_intersection(md, V2_ZERO, relative_velocity);
 
-                if (intersect_fraction != INFINITY) {
-                    Vector2 new_pos_a = v2_add(a->position, v2_mul_s(v2_mul_s(a->velocity, intersect_fraction), dt));
-                    Vector2 new_pos_b = v2_add(b->position, v2_mul_s(v2_mul_s(b->velocity, intersect_fraction), dt));
+                if (intersection.is_intersecting) {
+                    f32 t = intersection.time_of_impact;
+
+                    Vector2 new_pos_a = v2_add(a->position, v2_mul_s(v2_mul_s(a->velocity, t), dt));
+                    Vector2 new_pos_b = v2_add(b->position, v2_mul_s(v2_mul_s(b->velocity, t), dt));
 
                     // Move slightly in opposite direction to prevent getting stuck
                     a->position = v2_add(new_pos_a, v2_mul_s(v2_norm(a->velocity), -COLLISION_MARGIN));
@@ -270,15 +319,22 @@ static void world_update(GameWorld *world, const Input *input, f32 dt)
                     Vector2 normalized_rel_velocity = v2_norm(relative_velocity);
                     Vector2 tangent = { -normalized_rel_velocity.y, normalized_rel_velocity.x };
 
-                    a->velocity = v2_mul_s(tangent, v2_dot(a->velocity, tangent));
-                    b->velocity = v2_mul_s(tangent, v2_dot(b->velocity, tangent));
+                    if ((intersection.side_of_collision == RECT_SIDE_LEFT)
+                     || (intersection.side_of_collision == RECT_SIDE_RIGHT)) {
+                        a->velocity.x = 0.0f;
+                        b->velocity.x = 0.0f;
+                    } else {
+                        a->velocity.y = 0.0f;
+                        b->velocity.y = 0.0f;
+                    }
 
-                    velocity_this_frame = V2_ZERO;
+                    movement_fraction_left = 1.0f - t;
                 }
             }
         }
 
-        a->position = v2_add(a->position, v2_mul_s(velocity_this_frame, dt));
+        Vector2 to_move_this_frame = v2_mul_s(v2_mul_s(a->velocity, movement_fraction_left), dt);
+        a->position = v2_add(a->position, to_move_this_frame);
     }
 }
 

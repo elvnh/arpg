6e70d3981d3ad9c844fae6eaeed619bd9fd88e68
elvin <135953306+elvnh@users.noreply.github.com>
2025-09-28 13:22:39 +0200

    Remove incorrect calculation

--- src/game/game.c
+++ src/game/game.c
@@ -199,14 +199,11 @@ static f32 entity_vs_tilemap_collision(PhysicsComponent *physics, ColliderCompon
     s32 min_tile_y = (s32)(min_y / TILE_SIZE);
     s32 max_tile_y = (s32)(max_y / TILE_SIZE);
 
-    // No need to consider tiles outside bounds
-    min_tile_x = MAX(min_tile_x, world->tilemap.min_x);
-    max_tile_x = MIN(max_tile_x, world->tilemap.max_x);
-    min_tile_y = MAX(min_tile_y, world->tilemap.min_y);
-    max_tile_y = MIN(max_tile_y, world->tilemap.max_y);
-
-    CollisionInfo closest_collision = {0};
-    f32 closest_collision_distance = INFINITY;
+    // TODO: fix this
+    /* min_tile_x = MAX(min_tile_x, world->tilemap.min_x); */
+    /* max_tile_x = MIN(max_tile_x, world->tilemap.max_x); */
+    /* min_tile_y = MAX(min_tile_y, world->tilemap.min_y); */
+    /* max_tile_y = MIN(max_tile_y, world->tilemap.max_y); */
 
     for (s32 y = min_tile_y - 1; y <= max_tile_y + 1; ++y) {
         for (s32 x = min_tile_x - 1; x <= max_tile_x + 1; ++x) {

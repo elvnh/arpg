784c1bc6d93d8cdc5665e6503a015a57fe0c9ea0
elvin <135953306+elvnh@users.noreply.github.com>
2025-10-22 07:38:05 +0200

    Only consider bounding box clipped to parent bounds in widget interactions

--- src/game/ui/ui_core.c
+++ src/game/ui/ui_core.c
@@ -211,10 +211,12 @@ static void calculate_widget_layout(Widget *widget, Vector2 offset, PlatformCode
 
 }
 
-static void calculate_widget_interactions(UIState *ui, Widget *widget, const Input *input)
+static void calculate_widget_interactions(UIState *ui, Widget *widget, const Input *input, Rectangle parent_bounds)
 {
+    Rectangle clipped_bounds = widget_get_clipped_bounding_box(widget, parent_bounds);
+
     for (Widget *child = list_head(&widget->children); child; child = child->next_sibling) {
-        calculate_widget_interactions(ui, child, input);
+        calculate_widget_interactions(ui, child, input, clipped_bounds);
 
         // If child widget is being interacted with, don't consider input further up the tree
         if (widget_is_hot(ui, child)) {
@@ -226,8 +228,7 @@ static void calculate_widget_interactions(UIState *ui, Widget *widget, const Inp
         return;
     }
 
-    Rectangle rect = widget_get_bounding_box(widget);
-    b32 mouse_inside = rect_contains_point(rect, input->mouse_position);
+    b32 mouse_inside = rect_contains_point(clipped_bounds, input->mouse_position);
 
     if (mouse_inside) {
         ui->hot_widget = widget->id;
@@ -235,7 +236,7 @@ static void calculate_widget_interactions(UIState *ui, Widget *widget, const Inp
 
     if (widget_is_hot(ui, widget)) {
         b32 clicked_inside = input_is_key_down(input, MOUSE_LEFT)
-            && rect_contains_point(rect, input->mouse_click_position);
+            && rect_contains_point(clipped_bounds, input->mouse_click_position);
 
         if (widget_has_flag(widget, WIDGET_CLICKABLE) && clicked_inside) {
             ui->active_widget = widget->id;
@@ -336,7 +337,7 @@ static void render_widget(UIState *ui, Widget *widget, RenderBatch *rb, const As
 #endif
 
     for (Widget *child = list_head(&widget->children); child; child = child->next_sibling) {
-        render_widget(ui, child, rb, assets, depth + 1);
+        render_widget(ui, child, rb, assets, depth + 1, widget_rect);
     }
 }
 
@@ -348,6 +349,7 @@ void ui_core_end_frame(UIState *ui, const struct Input *input, RenderBatch *rb, 
 
     // TODO: take as parameter
     Rectangle window_bounds = {v2(0, 0), v2(2000, 2000)};
+
     if (ui->root_widget) {
         calculate_widget_layout(ui->root_widget, V2_ZERO, platform_code, 0);
         calculate_widget_interactions(ui, ui->root_widget, input, window_bounds);

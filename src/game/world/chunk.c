0884dae86e9ab165ce925a16a76c5572ee075e1e
elvin <135953306+elvnh@users.noreply.github.com>
2026-02-11 20:47:44 +0100

    Function for getting all chunks inside a rectangle

--- src/game/world/chunk.c
+++ src/game/world/chunk.c
@@ -24,12 +24,12 @@ Chunks create_chunks_for_tilemap(Tilemap *tilemap, LinearArena *arena)
 
 Chunk *get_chunk_at_position(Chunks *chunks, Vector2 position)
 {
-    f32 chunk_pixel_size = (f32)(CHUNK_SIZE_IN_TILES * TILE_SIZE);
+    f32 chunk_world_size = (f32)(CHUNK_SIZE_IN_TILES * TILE_SIZE);
 
     Vector2 chunks_base_world_coords = tile_to_world_coords(chunks->chunk_grid_base_tile_coords);
 
-    s32 chunk_x = (s32)floorf((position.x - chunks_base_world_coords.x) / chunk_pixel_size);
-    s32 chunk_y = (s32)floorf((position.y - chunks_base_world_coords.y) / chunk_pixel_size);
+    s32 chunk_x = (s32)floorf((position.x - chunks_base_world_coords.x) / chunk_world_size);
+    s32 chunk_y = (s32)floorf((position.y - chunks_base_world_coords.y) / chunk_world_size);
 
     Chunk *result = 0;
 
@@ -39,23 +39,45 @@ Chunk *get_chunk_at_position(Chunks *chunks, Vector2 position)
 
     if (in_bounds) {
         ssize index = chunk_x + chunk_y * chunks->chunk_grid_dims.x;
+        ASSERT(index < chunks->chunk_count);
 
-        if (index < chunks->chunk_count) {
-            result = &chunks->chunks[index];
-        }
+        result = &chunks->chunks[index];
     }
 
     return result;
 }
 
-ChunkArray get_chunks_in_area(Chunks *chunks, Rectangle area, LinearArena *arena)
+ChunkPtrArray get_chunks_in_area(Chunks *chunks, Rectangle area, LinearArena *arena)
 {
-    (void)chunks;
-    (void)area;
-    (void)arena;
-    UNIMPLEMENTED;
+    ChunkPtrArray result = {0};
 
-    ChunkArray result = {0};
+    f32 chunk_world_size = (f32)(CHUNK_SIZE_IN_TILES * TILE_SIZE);
+
+    s32 begin_x = (s32)area.position.x;
+    s32 begin_y = (s32)area.position.y;
+    s32 end_x = begin_x + (s32)ceilf(area.size.x / chunk_world_size);
+    s32 end_y = begin_y + (s32)ceilf(area.size.y / chunk_world_size);
+
+    ssize horizontal_chunk_count = end_x - begin_x + 1;
+    ssize vertical_chunk_count = end_y - begin_y + 1;
+    ssize total_chunk_count = horizontal_chunk_count * vertical_chunk_count;
+
+    result.chunks = la_allocate_array(arena, Chunk *, total_chunk_count);
+
+    for (ssize x = 0; x < horizontal_chunk_count; ++x) {
+        for (ssize y = 0; y < vertical_chunk_count; ++y) {
+            Vector2 coords = {
+                area.position.x + (f32)x * chunk_world_size,
+                area.position.y + (f32)y * chunk_world_size
+            };
+
+            ASSERT(result.count < total_chunk_count);
+
+            Chunk *chunk = get_chunk_at_position(chunks, coords);
+            result.chunks[result.count++] = chunk;
+        }
+    }
+
 
     return result;
 }
--- src/game/world/chunk.h
+++ src/game/world/chunk.h
@@ -13,20 +13,19 @@ typedef struct Chunk {
 } Chunk;
 
 typedef struct Chunks {
-    // TODO: reuse ChunkArray
-    Chunk *chunks;
-    ssize chunk_count;
-    Vector2i chunk_grid_dims;
-    Vector2i chunk_grid_base_tile_coords;
+    Chunk      *chunks;
+    ssize       chunk_count;
+    Vector2i    chunk_grid_dims;
+    Vector2i    chunk_grid_base_tile_coords;
 } Chunks;
 
-typedef struct ChunkArray {
-    Chunk *chunks;
-    ssize chunk_count;
-} ChunkArray;
+typedef struct ChunkPtrArray {
+    Chunk **chunks;
+    ssize   count;
+} ChunkPtrArray;
 
 Chunks      create_chunks_for_tilemap(struct Tilemap *tilemap, struct LinearArena *arena);
 Chunk      *get_chunk_at_position(Chunks *chunks, Vector2 position);
-ChunkArray  get_chunks_in_area(Chunks *chunks, Rectangle area, struct LinearArena *arena);
+ChunkPtrArray  get_chunks_in_area(Chunks *chunks, Rectangle area, struct LinearArena *arena);
 
 #endif //CHUNK_H

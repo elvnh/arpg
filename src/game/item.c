1f4681f33400cfafa6b2a60464fcb29d72aa03a7
elvnh <135953306+elvnh@users.noreply.github.com.>
2026-06-14 23:24:04 +0200

    Getting base item info

--- src/game/item.c
+++ src/game/item.c
@@ -4,12 +4,32 @@
 #include "world/world.h"
 
 // TODO: make this system similar to spells
+BaseItemInfo get_base_item_info(BaseItemID id)
+{
+#define BASE_ITEM(name, spelling, inv_w, inv_h) {spelling, {inv_w, inv_h}, {0}, {0, 0}}
+    static BaseItemInfo item_infos[] = {BASE_ITEM_LIST};
+#undef BASE_ITEM
+
+    ASSERT(VALID_INDEX_FOR(id, item_infos));
+
+    BaseItemInfo result = item_infos[id];
+
+    return result;
+}
 
 Entity *make_entity_from_item(World *world, Item item)
 {
     EntityWithID entity_with_id =
         world_spawn_non_spatial_entity(world, FACTION_NEUTRAL, ENTITY_KIND_PERSISTENT);
     Entity *e = entity_with_id.entity;
+
+    BaseItemInfo item_info = get_base_item_info(item.base_item);
+
+    NameComponent *name = es_add_component(e, NameComponent);
+    *name = name_component(str_from_c_str(item_info.name));
+
+    InventoryStorable *inv_item = es_add_component(e, InventoryStorable);
+    inv_item->inventory_grid_size = item_info.inventory_size;
 
     BEGIN_EXHAUSTIVE_SWITCH;
     switch (item.base_item) {
@@ -18,8 +38,6 @@ Entity *make_entity_from_item(World *world, Item item)
             sprite->sprite.texture = texture_handle(ICE_SHARD_TEXTURE);
             sprite->sprite.color = RGBA32_WHITE;
             sprite->sprite.size = v2(32, 32);
-
-            /*InventoryStorable *inv_item = */ es_add_component(e, InventoryStorable);
         } break;
 
             INVALID_CASE(ITEM_COUNT);
--- src/game/item.h
+++ src/game/item.h
@@ -1,21 +1,37 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include "base/vector.h"
+#include "components/modifier.h"
+#include "platform/asset.h"
+
 struct World;
 struct Entity;
 
-#include "components/modifier.h"
+#define BASE_ITEM_LIST                          \
+    BASE_ITEM(SWORD, "Sword", 2, 3)
 
-// TODO: rename to BaseItemID
+// clang-format off
+#define BASE_ITEM(name, spelling, inv_w, inv_h) ITEM_##name,
 typedef enum {
-    ITEM_SWORD,
+    BASE_ITEM_LIST
     ITEM_COUNT,
 } BaseItemID;
+#undef BASE_ITEM
+// clang-format on
 
 typedef struct {
     BaseItemID base_item;
     ItemModifiers modifiers;
 } Item;
+
+typedef struct {
+    char *name;
+    Vector2i inventory_size;
+
+    TextureHandle sprite;
+    Vector2 sprite_size;
+} BaseItemInfo;
 
 struct Entity *make_entity_from_item(struct World *world, Item item);
 
